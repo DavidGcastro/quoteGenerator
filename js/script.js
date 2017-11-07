283f961ef64cc797f28dfa79f99128186a4b9a57
David <davidgcastro93@gmail.com>
2017-11-06 23:51:09 -0500

    comments added
    added comments

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
+//my object of arrays, includes objects without a year, and citation
 var quotes = [
   {
     quote: "This is Quote number 1. Quote number 1 is here",
@@ -37,34 +39,49 @@ var random;
 var message = "";
 var randomColor;
 
-
+//executes printQuote
 document.getElementById("loadQuote").addEventListener("click", printQuote, false);
+//executes changeColor
 document.getElementById("loadQuote").addEventListener("click", changeColor, false);
 
+
 function getRandomQuote() {
+  // get a random number, the length of the array being the max
   random = Math.floor(Math.random() * quotes.length);
+  // returns the random object at that index
   return quotes[random];
 }
 
 
 function printQuote() {
+  //assigned the return value to a variable
   var printMe = getRandomQuote();
+  
+  // since the quote property is Always present, make it the inital value of message
   message = "<p class = 'quote'" + ">" + printMe.quote + "</p>";
+  // adds another property that is always present, source, notice that there is no closing </p> tag
   message += "<p class = 'source'" + ">" + printMe.source;
+    //check if ther3 is a citation, if there is run this:
   if (printMe.citation !== undefined) {
+    //adds a span class that is nested in side the paragraph
     message += "<span class = 'citation'" + ">" + printMe.citation + "</span>";
   }
   if (printMe.year !== undefined) {
+    // if there is a year property do this
     message += "<span class = 'year'" + ">" + printMe.year + "</span>";
   }
+  // after all these checks are done, finally add the closing p tag
   message += "</p>";
+  //print message to screen
   document.getElementById("quote-box").innerHTML = message;
 }
-
+// run printQuote every 5 seconds
 setInterval(printQuote, 5000);
 
 function changeColor() {
+    
   var backGround =
+    // gets 3 randoms numbers 1-255, concats them to make a rgb value
     "rgb(" +
     Math.floor(Math.random() * 256) +
     "," +
@@ -72,6 +89,6 @@ function changeColor() {
     "," +
     Math.floor(Math.random() * 256) +
     ")";
-  console.log(backGround);
+  // changes the background to the new value
   document.body.style.background = backGround;
 }
